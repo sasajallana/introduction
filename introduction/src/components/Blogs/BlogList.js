1b507927df1b0215380ce21c72adcc3895417923
Setup <setup@Setups-MacBook-Pro.local>
2020-02-28 11:35:26 +0900

    INT-03 | rebase : mass deleting backup

--- introduction/src/components/Blogs/BlogList.js
+++ introduction/src/components/Blogs/BlogList.js
@@ -6,17 +6,29 @@ export default class BlogList extends React.Component {
     // this.state = {
       
     // }
+
+  }
+
+  parseData() {
+
   }
 
   render() {
     const { data, keyword } = this.props;
-    console.log(typeof data);
+    console.log(this.props.data);
+
     return (
       <div>
         <h1>{keyword}</h1>
-        <ul>
-          
-        </ul>
+          {data.map((blog) =>(
+        		<div class="card">
+        			<div class="card-body">
+        				<h5 class="card-title">{blog.id}</h5>
+        				<h6 class="card-subtitle mb-2 text-muted">{blog.headline}</h6>
+        				<p class="card-text">{blog.dateCreated}</p>
+        			</div>
+        		</div>
+        	))}
       </div>
     )
   }
