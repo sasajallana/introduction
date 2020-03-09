a9f36d8c7aa4cbd0de25d3702551dc960bdb6fd2
Setup <setup@Setups-MacBook-Pro.local>
2020-03-09 17:00:43 +0900

    INT-15 | doc : update documentation

---
+++ modules/registration/src/main/java/com/liferay/docs/registration/constants/RegistrationPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.docs.registration.constants;
+
+/**
+ * @author setup
+ */
+public class RegistrationPortletKeys {
+
+	public static final String REGISTRATION =
+		"com_liferay_docs_registration_RegistrationPortlet";
+
+}
---
+++ modules/registration/src/main/java/com/liferay/docs/registration/portlet/RegistrationPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.docs.registration.portlet;
+
+import com.liferay.docs.registration.constants.RegistrationPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author setup
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Registration",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + RegistrationPortletKeys.REGISTRATION,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class RegistrationPortlet extends MVCPortlet {
+}
